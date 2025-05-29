a3300664475a2d8117cdcb99b9fcd26137f00079
rajmanna <dev.rajm8923@gmail.com>
2025-05-29 13:38:32 +0530

    group-anagram-complete

--- leetcode/49-group-anagrams.cpp
+++ leetcode/49-group-anagrams.cpp
@@ -3,6 +3,28 @@ using namespace std;
 
 vector<vector<string>> groupAnagrams(vector<string> &strs)
 {
+    vector<string> temp(strs); // copy strs
+
+    // sort each string in strs
+    for (string &s : strs)
+        sort(s.begin(), s.end());
+
+    // store the index of group anagrams
+    unordered_map<string, vector<int>> mpp;
+    for (int i = 0; i < strs.size(); i++)
+        mpp[strs[i]].push_back(i);
+
+    vector<vector<string>> vs(mpp.size());
+    // create the final array
+    int j = 0;
+    for (auto it : mpp)
+    {
+        for (auto i : it.second)
+            vs[j].push_back(temp[i]);
+        j += 1;
+    }
+
+    return vs;
 }
 
 int main()
