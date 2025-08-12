bc02e3e0769fd4eb629f0f0694be5ab8a7c71881
rajmanna <dev.rajm8923@gmail.com>
2025-08-12 19:03:55 +0530

    493. Reverse Pairs (brute force)

---
+++ leetcode/493.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nline "\n"
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+int reversePairs(vector<int> &nums)
+{
+    int n = nums.size();
+    int pairs = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (nums[i] > 2 * nums[j])
+                pairs++;
+        }
+    }
+
+    return pairs;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll tt, n;
+    cin >> tt;
+
+    while (tt--)
+    {
+        cin >> n;
+        vector<int> a(n);
+        for (int &x : a)
+            cin >> x;
+
+        cout << reversePairs(a) << nline;
+    }
+    return 0;
+}
