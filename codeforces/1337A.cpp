3019367b0a2b9f7de63c1b74a634e3c4d547a95f
rajmanna <dev.rajm8923@gmail.com>
2025-08-12 19:18:44 +0530

    1337A. Ichimichi and Triangle

---
+++ codeforces/1337A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nline "\n"
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll tt, a, b, c, d;
+    cin >> tt;
+
+    while (tt--)
+    {
+        cin >> a >> b >> c >> d;
+        cout << b << " " << c << " " << c << nline;
+    }
+    return 0;
+}
