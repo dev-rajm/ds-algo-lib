96bd99412824df4d7db64ef5b136f352eaf42f3f
rajmanna <dev.rajm8923@gmail.com>
2025-08-15 12:37:10 +0530

    493. Reverse pairs (optimial)

--- leetcode/493.cpp
+++ leetcode/493.cpp
@@ -6,6 +6,68 @@ using namespace std;
 #define pb push_back
 #define all(x) (x).begin(), (x).end()
 
+int cnt = 0;
+
+void merge(vector<int> &arr, int low, int mid, int high)
+{
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+    int i;
+
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp.pb(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.pb(arr[right]);
+            right++;
+        }
+    }
+    while (left <= mid)
+    {
+        temp.pb(arr[left]);
+        left++;
+    }
+    while (right <= high)
+    {
+        temp.pb(arr[right]);
+        right++;
+    }
+
+    for (i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+
+void countPairs(vector<int> &arr, int low, int mid, int high)
+{
+    int right = mid + 1;
+    for (int i = low; i <= mid; i++)
+    {
+        while (right <= high && arr[i] > 2 * arr[right])
+            right++;
+
+        cnt += (right - (mid + 1));
+    }
+}
+
+void mergeSort(vector<int> &arr, int low, int high)
+{
+    if (low >= high)
+        return;
+    int mid = (low + high) / 2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
+    countPairs(arr, low, mid, high);
+    merge(arr, low, mid, high);
+}
+
 int reversePairs(vector<int> &nums)
 {
     int n = nums.size();
@@ -22,6 +84,13 @@ int reversePairs(vector<int> &nums)
     return pairs;
 }
 
+int reversePairs1(vector<int> &nums)
+{
+    int n = nums.size();
+    mergeSort(nums, 0, n - 1);
+    return cnt;
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -36,7 +105,7 @@ int main()
         for (int &x : a)
             cin >> x;
 
-        cout << reversePairs(a) << nline;
+        cout << reversePairs1(a) << nline;
     }
     return 0;
 }
