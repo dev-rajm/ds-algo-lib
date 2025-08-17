5ba2163e62c0529d948a5e0bc5eca614f02a646d
rajmanna <dev.rajm8923@gmail.com>
2025-08-17 12:38:15 +0530

    959A. Mahmoud and Ebah and the even-odd game

---
+++ codeforces/959A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nline "\n"
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll n;
+    cin >> n;
+
+    if (n % 2 == 0)
+        cout << "Mahmoud";
+    else
+        cout << "Ehab";
+
+    return 0;
+}
