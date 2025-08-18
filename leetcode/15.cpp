c29355153f51f4eef77dcdb3840668d500a61891
rajmanna <dev.rajm8923@gmail.com>
2025-08-18 21:12:15 +0530

    15. 3Sum (optimal)

--- leetcode/15.cpp
+++ leetcode/15.cpp
@@ -60,6 +60,50 @@ vector<vector<int>> threeSum1(vector<int> &nums)
     return ans;
 }
 
+// Optimal approach
+// T.C => O(n log n) + O(n^2)
+// S.C => O(1)
+vector<vector<int>> threeSum2(vector<int> &nums)
+{
+    vector<vector<int>> ans;
+    int n = nums.size();
+
+    sort(all(nums));
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i != 0 && nums[i] == nums[i - 1])
+            continue;
+
+        int j = i + 1;
+        int k = n - 1;
+        while (j < k)
+        {
+            int sum = nums[i] + nums[j] + nums[k];
+            if (sum < 0)
+            {
+                j++;
+            }
+            else if (sum > 0)
+            {
+                k--;
+            }
+            else
+            {
+                vector<int> temp = {nums[i], nums[j], nums[k]};
+                ans.pb(temp);
+                j++, k--;
+                while (j < k && nums[j] == nums[j - 1])
+                    j++;
+                while (j < k && nums[k] == nums[k + 1])
+                    k--;
+            }
+        }
+    }
+
+    return ans;
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -74,7 +118,7 @@ int main()
         for (int &x : a)
             cin >> x;
 
-        vector<vector<int>> ans = threeSum1(a);
+        vector<vector<int>> ans = threeSum2(a);
         for (auto x : ans)
         {
             for (auto y : x)
