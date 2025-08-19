f6bbd8c7506d5e1a210efbe7b8d48b5e64d6469b
rajmanna <dev.rajm8923@gmail.com>
2025-08-19 12:40:16 +0530

    15. FourSum (optimal)

--- leetcode/18.cpp
+++ leetcode/18.cpp
@@ -70,6 +70,54 @@ vector<vector<int>> fourSum1(vector<int> &nums, int target)
     return ans;
 }
 
+// Optimal approach
+vector<vector<int>> fourSum2(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    vector<vector<int>> ans;
+
+    sort(all(nums));
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i != 0 && nums[i] == nums[i - 1])
+            continue;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (j != i + 1 && nums[j] == nums[j - 1])
+                continue;
+            int k = j + 1;
+            int l = n - 1;
+            while (k < l)
+            {
+                ll sum = nums[i] + nums[j];
+                sum += nums[k];
+                sum += nums[l];
+                if (sum < target)
+                {
+                    k++;
+                }
+                else if (sum > target)
+                {
+                    l--;
+                }
+                else
+                {
+                    ans.pb({nums[i], nums[j], nums[k], nums[l]});
+                    k++;
+                    l--;
+                    while (k < l && nums[k] == nums[k - 1])
+                        k++;
+                    while (k < l && nums[l] == nums[l + 1])
+                        l--;
+                }
+            }
+        }
+    }
+
+    return ans;
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -84,7 +132,7 @@ int main()
         for (int &x : a)
             cin >> x;
 
-        vector<vector<int>> ans = fourSum1(a, target);
+        vector<vector<int>> ans = fourSum2(a, target);
         for (auto v : ans)
         {
             for (auto x : v)
