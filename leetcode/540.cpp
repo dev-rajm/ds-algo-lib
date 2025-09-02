d1ebfaa659ea46fc2827b01924b88a71ab45ca96
rajmanna <dev.rajm8923@gmail.com>
2025-09-02 12:09:17 +0530

    540 (optimal)

--- leetcode/540.cpp
+++ leetcode/540.cpp
@@ -34,6 +34,40 @@ int singleNonDuplicate(vector<int> &nums)
     }
 }
 
+// T.C => O(log2n)
+// S.C => O(1)
+int singleNonDuplicate1(vector<int> &nums)
+{
+    int n = nums.size();
+    if (n == 1)
+        return nums[0];
+    if (nums[0] != nums[1])
+        return nums[0];
+    if (nums[n - 1] != nums[n - 2])
+        return nums[n - 1];
+
+    int low = 1, high = n - 2;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+        {
+            return nums[mid];
+        }
+        // left half
+        if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || (mid % 2 == 0 && nums[mid] == nums[mid + 1]))
+        {
+            low = mid + 1;
+        }
+        // right half
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -48,7 +82,7 @@ int main()
         for (int &x : arr)
             cin >> x;
 
-        cout << singleNonDuplicate(arr) << nline;
+        cout << singleNonDuplicate1(arr) << nline;
     }
     return 0;
 }
