95597d9fcbadf51b023039749ac6fbee605b8793
rajmanna <dev.rajm8923@gmail.com>
2025-09-06 16:08:49 +0530

    50A. Domino pailing

---
+++ codeforces/50A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nline "\n"
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int m, n, i = 1;
+    cin >> m >> n;
+
+    int size = m * n;
+    cout << (int)size / 2;
+    return 0;
+}
