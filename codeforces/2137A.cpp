9eaeca20cb8d0391697b64a9de7c927a866cc1ee
rajmanna <dev.rajm8923@gmail.com>
2025-09-09 12:08:03 +0530

    Collatz conjecture

---
+++ codeforces/2137A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nline "\n"
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll tt, k, x;
+    cin >> tt;
+
+    while (tt--)
+    {
+        cin >> k >> x;
+        cout << x * (1 << k) << nline;
+    }
+    return 0;
+}
