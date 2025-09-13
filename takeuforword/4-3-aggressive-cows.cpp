e9da59cf043bb7365ab6e8ec03d1e1d1f7924932
rajmanna <dev.rajm8923@gmail.com>
2025-09-13 17:06:31 +0530

    aggressive cows (optimal)

--- takeuforword/4-3-aggressive-cows.cpp
+++ takeuforword/4-3-aggressive-cows.cpp
@@ -50,6 +50,27 @@ int func(int arr[], int n, int k)
     }
 }
 
+// T.C => O(log (max - min)) * O(n)
+int opFunc(int arr[], int n, int k)
+{
+    sort(arr, arr + n);
+    int low = 1, high = arr[n - 1] - arr[0];
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int result = canPlace(arr, n, mid, k);
+        if (result)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return high;
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -66,7 +87,7 @@ int main()
             cin >> arr[i];
         }
 
-        cout << func(arr, n, k) << nline;
+        cout << opFunc(arr, n, k) << nline;
     }
     return 0;
 }
