f085142fc3a10a20bf0977d13196e918f2ed0c9b
rajmanna <dev.rajm8923@gmail.com>
2025-09-23 14:03:19 +0530

    bowwow and the timetable

--- codeforces/1204A.cpp
+++ codeforces/1204A.cpp
@@ -1,19 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define nline "\n"
-#define pb push_back
-#define all(x) (x).begin(), (x).end()
-
-int main()
+void solve()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
     string s;
     cin >> s;
 
-    ll n = stoll(s, nullptr, 2);
-    cout << ceil((double)log(n) / (double)log(4));
+    int set_bit_count = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '1')
+            set_bit_count++;
+        if (set_bit_count > 1)
+            break;
+    }
+
+    if (set_bit_count == 0)
+    {
+        cout << "0";
+    }
+    else if (set_bit_count == 1)
+    {
+        cout << ceil((double)(s.size() - 1) / (double)2);
+    }
+    else
+    {
+        cout << ceil((double)s.size() / (double)2);
+    }
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
     return 0;
 }
