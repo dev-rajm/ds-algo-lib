ca51afb1c4ff359ef8a8a0532f7e58fcc609ea24
rajmanna <dev.rajm8923@gmail.com>
2025-10-01 10:55:48 +0530

    630C. Lucky number

---
+++ codeforces/630C.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    cout << (2LL << n) - 2;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
