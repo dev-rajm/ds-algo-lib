1ddcbb6034552e202dc06fecee61ce4bdb92208b
rajmanna <dev.rajm8923@gmail.com>
2025-10-01 14:41:12 +0530

    1038B. Fixed Logi

--- codeforces/1038B.cpp
+++ codeforces/1038B.cpp
@@ -11,18 +11,16 @@ void solve()
     }
     else
     {
-        int t = (n * (n + 1)) / 2;
-        int k = ceil((double)n / (double)2);
         cout << "Yes\n";
-        cout << k << " ";
-        for (int i = 1; i <= k; i++)
+        int k = (n % 2 == 0) ? (n / 2) : (n + 1) / 2;
+        cout << "1 " << k << "\n";
+        cout << n - 1 << " ";
+        for (int i = 1; i <= n; i++)
         {
-            cout << i << " ";
-        }
-        cout << "\n";
-        cout << n - k << " ";
-        for (int i = k + 1; i <= n; i++)
-        {
+            if (i == k)
+            {
+                continue;
+            }
             cout << i << " ";
         }
     }
