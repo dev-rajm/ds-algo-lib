cc3567e47f12376125893011f3c3e2b44c2e29fb
rajmanna <dev.rajm8923@gmail.com>
2025-10-06 15:36:36 +0530

    2155A. El fucho

---
+++ codeforces/2155A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n;
+  cin >> n;
+
+  cout << 2*(n-1) << "\n";
+}
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    solve();
+  }
+
+  return 0;
+}
