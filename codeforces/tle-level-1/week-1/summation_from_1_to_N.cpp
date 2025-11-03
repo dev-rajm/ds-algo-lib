d816822517c79c9601c6bb7becd259b03444151f
rajmanna <dev.rajm8923@gmail.com>
2025-11-03 11:53:35 +0530

    tle-l1/summation from 1 to N

---
+++ codeforces/tle-level-1/week-1/summation_from_1_to_N.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  long long N;
+  cin >> N;
+
+  cout << (N * 1ll * (N+1))/2;
+}
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  solve();
+
+  return 0;
+}
