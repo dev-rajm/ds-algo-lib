97c25acd03f1e03843804988fa347d31b4f09042
rajmanna <dev.rajm8923@gmail.com>
2025-11-03 11:57:10 +0530

    tle-l1/welcome for you with conditions

---
+++ codeforces/tle-level-1/week-1/welcome_for_you_with_conditions.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int A, B;
+  cin>>A>>B;
+  if(A>=B) cout << "Yes";
+  else cout << "No";
+}
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  solve();
+
+  return 0;
+}
