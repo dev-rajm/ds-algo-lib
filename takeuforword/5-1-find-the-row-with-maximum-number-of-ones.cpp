fdc2a1a9c1cf71ad797b2647650ae8f6bb0b164e
rajmanna <dev.rajm8923@gmail.com>
2025-11-14 13:55:06 +0530

    tuf/update: find the row with maximum number of 1's

--- takeuforword/5-1-find-the-row-with-maximum-number-of-ones.cpp
+++ takeuforword/5-1-find-the-row-with-maximum-number-of-ones.cpp
@@ -1,32 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int lowerBound(vector<int> &a, int n, int x) {
+  int l = 0, h = n - 1;
+  int ans = n;
+  while (l <= h) {
+    int mid = (l + h) / 2;
+    if (a[mid] >= x) {
+      ans = mid;
+      h = mid - 1;
+    } else {
+      l = mid + 1;
+    }
+  }
+  return ans;
+}
+
 void solve() {
   int n, m;
   cin >> n >> m;
   vector<vector<int>> a(n, vector<int>(m));
-  for(int i=0; i<n; i++) {
-    for(int j=0; j<m; j++) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
       cin >> a[i][j];
     }
   }
 
   int count = 0;
-  int idx = 0;
-  for(int i=0; i<n; i++) {
-    int cnt = 0;
-    for(int j=0; j<m; j++) {
-      if(a[i][j] == 1) {
-        cnt++;
-      }
-    }
-    if(cnt > count) {
+  int idx = -1;
+  for (int i = 0; i < n; i++) {
+    int cnt = m - lowerBound(a[i], m, 1);
+    if (cnt > count) {
       count = cnt;
       idx = i;
     }
   }
 
-  cout << (count != 0 ? idx : -1);
+  cout << idx;
 }
 
 int32_t main() {
