c389c7ca4116c7e163a89f15a791454830dd7bb9
rajmanna <dev.rajm8923@gmail.com>
2025-11-19 15:08:53 +0530

    "900/comparison_string

--- codeforces/900/comparison_string.cpp
+++ codeforces/900/comparison_string.cpp
@@ -11,8 +11,8 @@ void solve() {
 
   int stack = 1;
   int max_stack = 1;
-  for(int i=0; i<n-1; i++) {
-    if(s[i] == s[i+1]) {
+  for (int i = 0; i < n - 1; i++) {
+    if (s[i] == s[i + 1]) {
       stack++;
     } else {
       stack = 1;
