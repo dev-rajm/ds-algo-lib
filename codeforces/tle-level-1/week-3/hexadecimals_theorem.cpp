88b2c28bee2b5f3c8246e12d0fed8d80336a6ef3
rajmanna <dev.rajm8923@gmail.com>
2025-11-20 13:14:01 +0530

    tle-l1/hexadecimals theorem

---
+++ codeforces/tle-level-1/week-3/hexadecimals_theorem.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+void solve() {
+  int n;
+  cin >> n;
+  cout << "0 0 " << n;
+}
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  solve();
+
+  return 0;
+}
