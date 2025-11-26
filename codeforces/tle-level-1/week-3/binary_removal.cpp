7aba897d336f5fdfadccb479a94e224815745df6
rajmanna <dev.rajm8923@gmail.com>
2025-11-26 13:51:33 +0530

    tle-l1/binary removal

---
+++ codeforces/tle-level-1/week-3/binary_removal.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+void solve() {
+  string s;
+  cin >> s;
+  int i = s.find("11");
+  int j = s.rfind("00");
+  cout << (i != -1 && j != -1 && i < j ? "NO" : "YES") << "\n";
+}
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    solve();
+  }
+
+  return 0;
+}
