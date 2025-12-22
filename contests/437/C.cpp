0c242d1df09c23a41a808c809ad70b135a1c2bf9
rajmanna <dev.rajm8923@gmail.com>
2025-12-22 16:58:54 +0530

    update 437

--- contests/437/C.cpp
+++ contests/437/C.cpp
@@ -5,20 +5,31 @@ using ll = long long;
 using ld = long double;
 
 void solve() {
-  ll n;
+  int n;
   cin >> n;
   vector<pair<ll, ll>> wp(n);
   for (int i = 0; i < n; i++) {
     ll w, p;
     cin >> w >> p;
-    wp.push_back({w, p});
+    wp[i] = {w, p};
   }
 
-  sort(wp.begin(), wp.end(), [](const pair<ll, ll> &a, const pair<ll, ll> &b) {
-    return a.second < b.second;
-  });
+  sort(wp.begin(), wp.end());
 
-  
+  const auto maxp = max_element(wp.begin(), wp.end(),
+                          [](const pair<ll, ll> &a, const pair<ll, ll> &b) {
+                            return a.second < b.second;
+                          });
+
+  ll sum = 0;
+  for (int i = 0; i < n; i++) {
+    if (sum <= maxp->second) {
+      sum += wp[i].first;
+    } else {
+      cout << i + 1 << "\n";
+      break;
+    }
+  }
 }
 
 int32_t main() {
