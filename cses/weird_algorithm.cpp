f9af50363f382f1a2bbb8a0ba7b1f7bfbdf077af
rajmanna <dev.rajm8923@gmail.com>
2025-12-23 11:57:21 +0530

    cses/weird algorithm, missing number

--- cses/weird_algorithm.cpp
+++ cses/weird_algorithm.cpp
@@ -5,21 +5,24 @@ using ll = long long;
 using ld = long double;
 
 void solve() {
-  int n;
+  ll n;
   cin >> n;
-  ll sum = 0;
-  for (int i = 0; i < n - 1; i++) {
-    int s;
-    cin >> s;
-    sum += s;
+  cout << n << " ";
+  while(n != 1) {
+    if(n & 1) {
+      n *= 3;
+      n += 1;
+    } else {
+      n /= 2;
+    }
+    cout << n << " ";
   }
-  ll esum = n*1LL*(n*1LL+1)/(2*1LL);
-  cout << esum - sum;
 }
 
 int32_t main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
+
   solve();
 
   return 0;
