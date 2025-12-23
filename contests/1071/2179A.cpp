50404006d360860cdb1e33a9bb2098018f7cd952
rajmanna <dev.rajm8923@gmail.com>
2025-12-23 20:46:52 +0530

    cf/2179A

---
+++ contests/1071/2179A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+void solve() {
+  int k, x;
+  cin >> k >> x;
+  cout << k * x + 1 << "\n";
+}
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    solve();
+  }
+
+  return 0;
+}
